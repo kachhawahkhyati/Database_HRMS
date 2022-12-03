433887d4744d0c748537fe5c08e589859989e76c
kachhawahkhyati <kachhawah.k@northeastern.edu>
2022-12-02 19:23:22 -0500

    Created Package

--- application_admin_hrmsdb_FUNCTIONS.sql
+++ application_admin_hrmsdb_FUNCTIONS.sql
@@ -11,13 +11,13 @@ END;
 select total_Customers from dual;
 select * from customer;
 
-
+drop function total_customers;
 
 CREATE OR REPLACE FUNCTION Total_Rooms_Available 
 RETURN number IS  
    rooms_available number(2) := 0;  
 BEGIN  
-   SELECT count(*) into rooms_available  
+   SELECT count(room_no) into rooms_available  
    FROM room where availability_status='T';  
     RETURN rooms_available;  
 END;  
@@ -26,7 +26,7 @@ END;
 select Total_Rooms_Available from dual;
 select * from room;
 
-
+drop function Total_Rooms_Available;
 
 
 create or replace function get_room_Type(input_room_no in number)
@@ -42,8 +42,26 @@ begin
 end;
 /
 
-select get_room_Type(310) from dual;
+select get_room_Type(316) from dual;
 
+drop function get_room_type;
+
+
+
+
+CREATE OR REPLACE FUNCTION total_Profit  
+RETURN decimal IS  
+   total decimal := 0.0;  
+BEGIN  
+   SELECT sum(amount) into total  
+   FROM transactions;  
+    RETURN total;  
+END;  
+/  
+
+select total_Profit from dual;
+select * from transactions;
+drop function total_profit;
 
 create or replace function is_room_Available(input_room_no in number)
 return VARCHAR
@@ -60,6 +78,9 @@ end;
 select is_room_Available(310) from dual;
 select * from room;
 
+drop function is_room_Available;
+
+select all_functions from dual;
 
 SELECT 
     owner,
@@ -78,14 +99,11 @@ begin
 insert into customer values(default,1,name,email,ssn,country);    
 end;    
 /
-
-
-
 declare
 ex exception;
 pragma exception_init(ex,-1);
 BEGIN
-insertcustomer('Khyati Kachhawah','khyati@gmail.com','JKD$1','India');
+insertcustomer('Aishwarya Katkar','aishwarya@gmail.com','AISH99$1','India');
 dbms_output.put_line('record inserted successfully');  
   exception
   when ex then
@@ -97,7 +115,7 @@ END;
 
 select * from customer;
 
-
+drop procedure insertcustomer;
 
 
 CREATE PROCEDURE remove_room(room_no NUMBER) AS
@@ -108,7 +126,6 @@ CREATE PROCEDURE remove_room(room_no NUMBER) AS
    tot_rooms := tot_rooms - 1;
    END;
 /
-
 DECLARE
         not_exists      EXCEPTION;
         PRAGMA          EXCEPTION_INIT(not_exists, -2443);
@@ -119,10 +136,12 @@ remove_room(310);
      THEN NULL; 
 END;    
 /    
+  
 
+EXECUTE remove_room(310);
 select * from room;
 
-
+drop procedure remove_room;
 
 CREATE or replace PROCEDURE display_PaymentDetails(input_customer_id NUMBER) AS
 pay_method payment%ROWTYPE;
@@ -142,7 +161,7 @@ END;
 
 EXECUTE display_PaymentDetails(1); 
 
-
+drop procedure display_PaymentDetails;
 
 
 SELECT 
@@ -152,6 +171,20 @@ FROM
     all_objects
 WHERE
     object_type = 'PROCEDURE' AND owner='APPLICATION_ADMIN';
+
+CREATE OR REPLACE TRIGGER display_price_changes 
+AFTER DELETE OR UPDATE ON room_type
+FOR EACH ROW 
+WHEN (NEW.room_type_ID > 0) 
+DECLARE 
+   price_diff number; 
+BEGIN 
+   price_diff := :NEW.price  - :OLD.price; 
+   dbms_output.put_line('Old price: ' || :OLD.price); 
+   dbms_output.put_line('New price: ' || :NEW.price); 
+   dbms_output.put_line('Price difference: ' || price_diff); 
+END; 
+/ 
 
 CREATE OR REPLACE TRIGGER Print_price_changes
   BEFORE DELETE OR INSERT OR UPDATE ON room_type
@@ -175,15 +208,31 @@ select * from room_type;
 
 
 
+CREATE OR REPLACE PACKAGE hotel_mgmt AS 
+   FUNCTION total_Customers 
+      RETURN NUMBER; 
+   FUNCTION Total_Rooms_Available
+      RETURN NUMBER; 
+   function get_room_Type(input_room_no number)
+   return VARCHAR;
+   function is_room_Available(input_room_no number)
+   return VARCHAR;
+   PROCEDURE INSERTCUSTOMER(name IN VARCHAR, email in varchar, ssn in varchar, country in varchar);   
+   PROCEDURE remove_room(room_no NUMBER); 
+   PROCEDURE display_PaymentDetails(input_customer_id NUMBER);
+END hotel_mgmt; 
+/ 
+
+
+
+
 CREATE OR REPLACE TRIGGER insert_room_type 
-AFTER INSERT ON room_type
+BEFORE INSERT ON room_type
 FOR EACH ROW
 BEGIN 
 insert into room_type values(default,'Breakfast, Dinner',21000.0,'Dorm');
 END; 
 / 
-
-select * from room_type;
 
 SELECT 
     owner,
@@ -192,10 +241,6 @@ FROM
     all_objects
 WHERE
     object_type = 'TRIGGER' AND owner='APPLICATION_ADMIN';
-    
 
 
-create index customer_name_index on customer(customer_name);
-
-select * from customer where customer_name like 'S%';
 
